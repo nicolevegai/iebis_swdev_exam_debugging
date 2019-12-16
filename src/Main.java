e39fa13ad35f937e2c4b898393dfefcfeca3449f
nicolevegai <mvega.ieu2017@student.ie.edu>
2019-12-16 12:15:41 +0100

    CLEAN CODE

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         String emailAddress = "john.doe.mis2016@ie.edu";
-        String formattedEmailAddress = emailAddress.replaceAll("\\.", "/"); // 1.  The problem here is that the "." are not being replaced by "/" by editining the the regex to "\\.", we solve this problem and the "." replace the "."
+        String formattedEmailAddress = emailAddress.replaceAll("\\.", "/"); 
 
 
 
@@ -13,7 +13,7 @@ public class Main {
         StringBuffer word = null;
 
 
-        switch (random.nextInt(3)) {        // 3. if the bound is only 2 it will only take the case 0 and 1 and ignore case 2 so we need to add the bound to 3
+        switch (random.nextInt(3)) {
             case 0:
                 word = new StringBuffer("Y");
                 break;
@@ -22,8 +22,7 @@ public class Main {
                 break;
             case 2:
                 word = new StringBuffer("T");
-                break; //2. the Y, F and I are chars and can not be added to a string so we need to convert them to strings.
-            //4. as there is no break in each case only the last case runs  so we need to add a break point after every case so if the random selects that case it breaks
+                break;
         }
 
         word.append('o');
